95ac5be26cc3574ba75eaebd794a39a739c6b552
Silviu <silviu.iordache92@gmail.com>
2019-06-14 11:32:42 +0300

    Final submit
    Finished the tutorial. Submitting a stable working version of this project.

---
+++ README.txt
@@ -0,0 +1,11 @@
+INITIAL SETUP
+1. Open cmd shell on the path of the project
+2. type: npm install (this should install all node modules specified in the package.json file.
+
+
+
+HOW TO RUN APPLICATION
+1. Open a cmd shell in the location of the project
+2. type: npm run dev
+3. Open 2nd cmd shell in the same location
+4. type: npm run start
--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,11 @@
-export const key = '7f34ed11d7823c35145ad0dbc42c402d';
+// silviu.iordache key
+//export const key = '7f34ed11d7823c35145ad0dbc42c402d'; 
+
+// bardikus key
+//export const key = 'f897c7d36dce80858a2cdda6bd119124';
+
+// silviu.costin.iordache key
+export const key = '9ee366fa1f922469f0132a7ce5e09dff';
+
+
 export const proxy = '';
